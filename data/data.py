5ad474ca37c3c649280b14d19f64122b0733ddd6
Helena Waldorf <helena@waldorf.de>
2025-01-26 20:53:38 +0100

    changes

--- data/data.py
+++ data/data.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 
+# Berlin data 
 # comma to point replacment 
 berlin_demo_data = pd.read_csv('data/preprocessed_data/berlin_districts_demographics_translated.csv')
 berlin_employ_data = pd.read_csv('data/preprocessed_data/berlin_districts_employment_translated.csv')
@@ -12,12 +13,11 @@ for column in berlin_demo_data.select_dtypes(include=['object']).columns:
 for column in berlin_employ_data.select_dtypes(include=['object']).columns:
     berlin_employ_data[column] = berlin_employ_data[column].str.replace(',', '.')
 
+# merging data
 merged_berlin_data = pd.merge(berlin_demo_data, berlin_employ_data, on = 'district_name', how ='inner')
-
-berlin_demo_data.to_csv('data/preprocessed_data/berlin_districts_demographics_translated_modified.csv', index=False)
-berlin_employ_data.to_csv('data/preprocessed_data/berlin_districts_employment_translated_modified.csv', index=False)
 merged_berlin_data.to_csv('data/preprocessed_data/berlin_districts_data.csv', index=False)
 
+# Hamburg data 
 # excel to csv 
 hamburg_data = pd.read_excel('data/preprocessed_data/hamburg_districts_data_translated.xlsx')
-hamburg_data.to_csv('data/preprocessed_data/hamburg_districts_data_translated.csv', index=False)
+hamburg_data.to_csv('data/preprocessed_data/hamburg_districts_data.csv', index=False)
